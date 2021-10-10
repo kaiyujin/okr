46d70f7c65314fadbc06a3a73fe72f932700de48
daisuke shiraishi <kaiyujin@gmail.com>
2021-10-10 23:31:17 +0900

    enable darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit', // add just-in-time
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // 未使用スタイルの除外
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'false' or 'media' or 'class'
   theme: {
     extend: {},
   },
